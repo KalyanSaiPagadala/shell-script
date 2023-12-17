675993e1b0876b1605227f3b5cba3e66a4f66d44
Sai Kalyan <pagadalasaikalyan7@gmail.com>
2023-12-17 18:27:49 +0530

    3rd change

--- variable4.sh
+++ variable4.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# this method is used when we want to maintain privacy of our information
+
 echo "please enter the username ::"
 
-read -s USERNAME
+read -s USERNAME. # read will automatically attach the value to the USERNAME and -s will hide the input coming from the terminal
 
 echo "please enter the password ::"
 
