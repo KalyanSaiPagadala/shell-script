9d1e5e5b48d5e86f70b111f7efc9338f50e3960d
Sai Kalyan <pagadalasaikalyan7@gmail.com>
2023-12-17 18:22:42 +0530

    3rd change

--- variable4.sh
+++ variable4.sh
@@ -2,6 +2,10 @@
 
 echo "please enter the username ::"
 
+read -s USERNAME
+
 echo "please enter the password ::"
+
+read -s PASSWORD
 
 echo "" username is :: $USERNAME , password is :: $PASSWORD" 
