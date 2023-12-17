b941f5deb9dae454d65fd5bf41e2caba874f2def
Sai Kalyan <pagadalasaikalyan7@gmail.com>
2023-12-17 14:32:15 +0530

    2nd-script

--- variable.sh
+++ variable.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo "kumar:: hi mark"
-echo "mark:: bye kumar"
-echo "mark:: ewwww"
-echo "kumar:: thuuuu mark"
+P1=kumar
+
+P2=mark
+
+echo "$P1:: hi $P2"
+echo "$P2:: bye $P1"
+echo "$P2:: ewwww"
+echo "$P1:: thuuuu $P2"
